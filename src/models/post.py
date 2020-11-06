d4401a4c75143297c1b36741dab730e2926403e3
Daniel Bailo <danbailoufms@gmail.com>
2020-11-06 03:32:41 -0300

    methods to get data

--- src/models/post.py
+++ src/models/post.py
@@ -2,13 +2,13 @@ from src.database import Database
 
 class Post:
 
-    def __init__(self, blog_id, title, content, author, date, id_post):
+    def __init__(self, blog_id, title, content, author, date, post_id):
         self.blog_id = blog_id
         self.title = title
         self.content = content
         self.author = author
         self.date = date
-        self.id_post = id_post
+        self.post_id = post_id
 
     def save_to_mongo(self):
         Database.insert(collection="posts",
@@ -16,10 +16,19 @@ class Post:
 
     def json(self):
         return {
-            "id_post": self.id_post,
+            "post_id": self.post_id,
             "blog_id": self.blog_id,
             "author": self.author,
             "title": self.title,
             "content": self.content,
             "date": self.date,
         }
+
+    @staticmethod
+    def from_mongo(post_id):
+        return Database.find_one(collection="posts", query={"post_id": post_id})
+
+    @staticmethod
+    def from_blog(blog_id):
+        return [post for post in Database.find(collection="posts", query={"blog_id": blog_id})]
+
